2bcf134e7adfce899602fafa9d19a15d2c1dd5da
AdhamNour <adhamnourelwaffaa@gmail.com>
2022-09-09 23:48:13 +0200

    finished the elements of the onboarding screen

--- app/src/main/java/com/answ/anshoehouse/onboarding/OnBoarding.kt
+++ app/src/main/java/com/answ/anshoehouse/onboarding/OnBoarding.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.answ.anshoehouse.R
+import com.answ.anshoehouse.databinding.FragmentOnBoardingBinding
+import com.bumptech.glide.Glide
+import java.net.URL
 
 class OnBoarding : Fragment() {
 
@@ -14,18 +18,29 @@ class OnBoarding : Fragment() {
         fun newInstance() = OnBoarding()
     }
 
-    private lateinit var viewModel: OnBoardingViewModel
+    private lateinit var binding: FragmentOnBoardingBinding;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_on_boarding, container, false)
+        val circularProgressDrawable = context?.let { CircularProgressDrawable(it) }
+        circularProgressDrawable!!.strokeWidth = 5f
+        circularProgressDrawable!!.centerRadius = 100f
+        circularProgressDrawable!!.start()
+        binding = FragmentOnBoardingBinding.inflate(inflater, container, false)
+        Glide.with(this).load(URL("https://source.unsplash.com/random/300x300/?shoe")).fitCenter()
+            .placeholder(circularProgressDrawable)
+            .into(binding.imageView)
+        Glide.with(this).load(URL("https://source.unsplash.com/random/300x300/?shoes")).fitCenter()
+            .placeholder(circularProgressDrawable)
+            .into(binding.imageView1)
+        Glide.with(this).load(URL("https://source.unsplash.com/random/300x400/?shoe")).fitCenter()
+            .placeholder(circularProgressDrawable)
+            .into(binding.imageView2)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(OnBoardingViewModel::class.java)
-    }
+
 
 }
