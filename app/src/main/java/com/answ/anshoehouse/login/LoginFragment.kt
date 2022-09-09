e30d0cfeecf311086c2f9754ed3af68e49e703d9
AdhamNour <adhamnourelwaffaa@gmail.com>
2022-09-09 20:43:52 +0200

    Finished Log in Screen

--- app/src/main/java/com/answ/anshoehouse/login/LoginFragment.kt
+++ app/src/main/java/com/answ/anshoehouse/login/LoginFragment.kt
@@ -2,11 +2,15 @@ package com.answ.anshoehouse.login
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import com.answ.anshoehouse.R
+import com.answ.anshoehouse.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
@@ -15,18 +19,34 @@ class LoginFragment : Fragment() {
     }
 
     private lateinit var viewModel: LoginViewModel
+    private lateinit var binding: FragmentLoginBinding;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.loginViewModel = viewModel
+        binding.emailTextEdit.doOnTextChanged { text, start, before, count ->
+            viewModel.setEmail(text.toString())
+        }
+        binding.passwordEditText.doOnTextChanged { text, start, before, count ->
+            viewModel.setPassword(
+                text.toString()
+            )
+        }
+        binding.loginButton.setOnClickListener {
+            if (viewModel.login())
+                Toast.makeText(
+                    requireContext(),
+                    "Login success", Toast.LENGTH_SHORT
+                ).show()
+            else
+                Toast.makeText(
+                    requireContext(), "Login Failed", Toast.LENGTH_SHORT
+                ).show()
+        }
+        return binding.root
     }
-
 }
--- app/src/main/java/com/answ/anshoehouse/login/LoginViewModel.kt
+++ app/src/main/java/com/answ/anshoehouse/login/LoginViewModel.kt
@@ -1,7 +1,36 @@
 package com.answ.anshoehouse.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _email = MutableLiveData<String>()
+    val email: LiveData<String>
+        get() = _email
+    private val _password = MutableLiveData<String>()
+    val password: LiveData<String>
+        get() = _password
+
+    init {
+        _email.value = ""
+        _password.value = ""
+    }
+
+    fun setEmail(email: String) {
+        _email.value = email
+    }
+
+    fun setPassword(password: String) {
+        _password.value = password
+    }
+
+    fun login(): Boolean {
+        if (email.value?.length == 0)
+            return false
+        if (password.value?.length == 0)
+            return false
+        return true
+    }
 }
