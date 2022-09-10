fa8d1f317500e6a99c499e183b0a24980c68abb1
AdhamNour <adhamnourelwaffaa@gmail.com>
2022-09-10 22:29:28 +0200

    finished Shoe List Screen

--- app/src/main/java/com/answ/anshoehouse/shoelist/ShoeListFragment.kt
+++ app/src/main/java/com/answ/anshoehouse/shoelist/ShoeListFragment.kt
@@ -2,11 +2,25 @@ package com.answ.anshoehouse.shoelist
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.core.view.marginBottom
+import androidx.core.view.setMargins
+import androidx.core.view.setPadding
+import androidx.lifecycle.Observer
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.answ.anshoehouse.R
+import com.answ.anshoehouse.databinding.FragmentShoeListBinding
+import com.bumptech.glide.Glide
+import java.net.URL
 
 class ShoeListFragment : Fragment() {
 
@@ -15,18 +29,80 @@ class ShoeListFragment : Fragment() {
     }
 
     private lateinit var viewModel: ShoeListViewModel
+    private lateinit var binding: FragmentShoeListBinding;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_shoe_list, container, false)
+        binding = FragmentShoeListBinding.inflate(inflater, container, false);
+        viewModel = ViewModelProvider(this).get(ShoeListViewModel::class.java)
+        viewModel.shoesList.forEach {
+
+
+            binding.items.addView(
+                getShoeLinearLayout(
+                    getShoeDataLinearLayout(
+                        getTextView(it.name),
+                        getTextView(it.price.toString())
+                    ), getImageView()
+                )
+            )
+        }
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ShoeListViewModel::class.java)
-        // TODO: Use the ViewModel
+    fun getTextView(str: String): TextView {
+        val textView = TextView(context)
+        textView.text = str
+        textView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        return textView
     }
+
+    fun getImageView(): ImageView {
+        val imageView = ImageView(context)
+        imageView.layoutParams = ViewGroup.LayoutParams(300, 300);
+        val circularProgressDrawable = CircularProgressDrawable(requireContext())
+        circularProgressDrawable.strokeWidth = 5f
+        circularProgressDrawable.centerRadius = 100f
+        circularProgressDrawable.start()
+        Glide.with(this).load(URL("https://source.unsplash.com/random/300x300/?shoe"))
+            .fitCenter()
+            .placeholder(circularProgressDrawable)
+            .into(imageView)
+        return imageView;
+    }
+
+    fun getShoeDataLinearLayout(name: TextView, price: TextView): LinearLayout {
+        val linearLayout = LinearLayout(context)
+        linearLayout.orientation = LinearLayout.VERTICAL
+        linearLayout.layoutParams = LinearLayout.LayoutParams(
+            0,
+            LinearLayout.LayoutParams.WRAP_CONTENT, 1f
+        )
+        linearLayout.addView(name)
+        linearLayout.addView(price)
+        return linearLayout
+    }
+
+    fun getShoeLinearLayout(data: LinearLayout, img: ImageView): LinearLayout {
+        val linearLayout = LinearLayout(context)
+        linearLayout.orientation = LinearLayout.HORIZONTAL
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+        )
+        params.setMargins(10)
+        linearLayout.layoutParams=params
+        linearLayout.gravity = Gravity.CENTER
+        linearLayout.addView(data)
+        linearLayout.addView(img)
+        return linearLayout
+    }
+
 
 }
--- app/src/main/java/com/answ/anshoehouse/shoelist/ShoeListViewModel.kt
+++ app/src/main/java/com/answ/anshoehouse/shoelist/ShoeListViewModel.kt
@@ -1,7 +1,25 @@
 package com.answ.anshoehouse.shoelist
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ShoeListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private lateinit var _shoesList: MutableList<Shoe>;
+    public val shoesList: MutableList<Shoe>
+    get() = _shoesList
+    init {
+        getShoes()
+    }
+
+    private fun getShoes(): Unit {
+        Log.d("getShoes", "Shoes list retrieved")
+        _shoesList = mutableListOf<Shoe>()
+        for (i in 0..10) {
+            _shoesList.add(Shoe("Show $i", i.toFloat()))
+        }
+    }
 }
+
+data class Shoe(val name: String, val price: Float)
