1f7a89e8fb12b32a9b0869d6da9aa998f0b22b92
Adham Nour <adhamnourelwaffaa@gmail.com>
2022-09-16 17:25:02 +0200

    project finished (I hope)

--- app/src/main/java/com/answ/anshoehouse/shoedetails/ShoeDetailsFragment.kt
+++ app/src/main/java/com/answ/anshoehouse/shoedetails/ShoeDetailsFragment.kt
@@ -13,31 +13,39 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.answ.anshoehouse.R
 import com.answ.anshoehouse.databinding.FragmentShoeDetailsBinding
+import com.answ.anshoehouse.shoelist.Shoe
 import com.answ.anshoehouse.shoelist.ShoeListViewModel
 
 class ShoeDetailsFragment : Fragment() {
 
-    private  val viewModel: ShoeDetailsViewModel by viewModels()
-    private val sharedViewModel:ShoeListViewModel by activityViewModels()
+    private val viewModel: ShoeDetailsViewModel by viewModels()
+    private val sharedViewModel: ShoeListViewModel by activityViewModels()
     private lateinit var binding: FragmentShoeDetailsBinding;
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentShoeDetailsBinding.inflate(inflater, container, false)
-        NavigationUI.setupActionBarWithNavController(this.activity as AppCompatActivity, findNavController())
-        binding.vm= viewModel
+        NavigationUI.setupActionBarWithNavController(
+            this.activity as AppCompatActivity,
+            findNavController()
+        )
+        binding.vm = viewModel
         binding.save.setOnClickListener {
-            if(viewModel.shoe.value!=null) {
-                Log.d("ShoeDetails",viewModel.shoe.value.toString())
-                sharedViewModel.addShoe(viewModel.shoe.value!!)
+            if (viewModel.name.value != null && viewModel.price.value != null) {
+                Log.d("ShoeDetails", viewModel.name.value ?: "null")
+                sharedViewModel.addShoe(
+                    Shoe(
+                        viewModel.name.value!!,
+                        viewModel.price.value!!.toFloat()
+                    )
+                )
                 findNavController().popBackStack()
+
             }
         }
         return binding.root
     }
-
-
 
 
 }
--- app/src/main/java/com/answ/anshoehouse/shoedetails/ShoeDetailsViewModel.kt
+++ app/src/main/java/com/answ/anshoehouse/shoedetails/ShoeDetailsViewModel.kt
@@ -1,17 +1,14 @@
 package com.answ.anshoehouse.shoedetails
 
+import androidx.databinding.Bindable
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.answ.anshoehouse.shoelist.Shoe
 
 class ShoeDetailsViewModel : ViewModel() {
-    private val _shoe: MutableLiveData<Shoe> = MutableLiveData()
-    public val shoe
-        get() = _shoe
+    val name = MutableLiveData<String>()
 
-    init {
-        _shoe.value = Shoe("",0f)
-    }
+    val price = MutableLiveData<String>()
 
-    
 }
